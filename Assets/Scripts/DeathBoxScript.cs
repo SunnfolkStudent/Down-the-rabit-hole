76beb3fb272d8afc00c33feb1de4787c7d62077d
IsItPenguins <eirik.risegabr@gmail.com>
2023-10-06 15:59:06 +0200

    FIXED

---
+++ Assets/Scripts/DeathBoxScript.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathBoxScript : MonoBehaviour
+{
+    
+    private SceneController sceneController;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SceneManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
