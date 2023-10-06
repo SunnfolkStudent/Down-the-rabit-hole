a1f9fcbd4d6b22f48adaedf2e447b8215ef2f712
Ytsharko <eliashilvardin@gmail.com>
2023-10-06 11:01:50 +0200

    Updated Scenes & Scenecontroler

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,5 +6,20 @@ public class SceneController : MonoBehaviour
     public void Loadscene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.CompareTag("LoadNextLevel"))
+        {
+            if (SceneManager.sceneCountInBuildSettings > SceneManager.GetActiveScene().buildIndex)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+            }
+        }
     }
 }
